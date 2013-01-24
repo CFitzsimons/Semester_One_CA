e50d0c1d7c10bb9aa173e389d2b085a5e61d9a93
Colin Fitzsimons <colin.altered@gmail.com>
2013-01-24 17:22:58 +0000

    Updated to working Question 10
    This version of the question should operate correctly.  Again, if you see an error drop me a line and I'll fix it up.

--- Logic/Sample Paper/Question10.pl
+++ Logic/Sample Paper/Question10.pl
@@ -7,14 +7,11 @@ software_co(Name) :-
 	company(Name, software).
 	
 payroll(Comp, Sum) :-
-	staff(Comp, AllStaff),        %Generates a list of staff at Comp
-	getSum(AllStaff, Compare),    %Calls getSum method and stores into Compare
-	Compare == Sum.               %Preforms a compare between variables Compare and Sum
-	
-getSum([], _).                        %Stop method if list is empty.
+	staff(Comp, AllStaff),       	 %Generates a list of staff at Comp
+	compareSum(AllStaff, Sum, 0).    %Calls compareSum
 
-getSum([H|T], X) :-
-	H(_,_,Salery),                %I'm 99% sure this line is wrong but I don't know how to write it
-	Y is X,                       %Set Y as X
-	X is Salery+Y,                %Set X as H plus Y
-	getSum(T, X).                 %Invoke getSum, passing in new X.
+compareSum([], Sum, Total):-
+	Sum == Total.			 %Make Comparision and return true or false.
+compareSum([_|T], Sum, Total):-
+	Temp is Total + 1,		 %Everytime an element is found, add one to the total employees
+	compareSum(T, Sum, Temp).	 %Call compareSum again, with new Total and progressed list.
