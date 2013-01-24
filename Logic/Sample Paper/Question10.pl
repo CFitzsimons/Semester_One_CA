8418c9e7778ed403ba6f776333cdbd3e0d121310
Colin Fitzsimons <colin.altered@gmail.com>
2013-01-24 17:07:33 +0000

    Fixed a bug
    There is a massive error on line 17 but I have no idea how to fix it.  If anyone has an idea, drop me a line I'd love to know how to fix it.

--- Logic/Sample Paper/Question10.pl
+++ Logic/Sample Paper/Question10.pl
@@ -11,9 +11,10 @@ payroll(Comp, Sum) :-
 	getSum(AllStaff, Compare),    %Calls getSum method and stores into Compare
 	Compare == Sum.               %Preforms a compare between variables Compare and Sum
 	
-getSum([], _).                    %Stop method if list is empty.
+getSum([], _).                        %Stop method if list is empty.
 
 getSum([H|T], X) :-
+	H(_,_,Salery),                %I'm 99% sure this line is wrong but I don't know how to write it
 	Y is X,                       %Set Y as X
-	X is H+Y,                     %Set X as H plus Y
+	X is Salery+Y,                %Set X as H plus Y
 	getSum(T, X).                 %Invoke getSum, passing in new X.
